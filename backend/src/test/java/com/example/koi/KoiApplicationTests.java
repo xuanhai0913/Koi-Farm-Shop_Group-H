a4f9f022d12d337939a455ab14ed78dd8c474bbf
Nguyễn Xuân Hải <73636134+xuanhai0913@users.noreply.github.com>
2024-10-25 09:04:39 +0700

    Update Back-end

---
+++ backend/src/test/java/com/example/koi/KoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.koi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
